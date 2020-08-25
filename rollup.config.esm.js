ac28146782ac418666b4547dbc60db8a42808fb4
CJ-Zheng1023 <CJ_Zheng1023@hotmail.com>
2020-08-25 18:26:10 +0800

    base

---
+++ rollup.config.esm.js
@@ -0,0 +1,8 @@
+export default {
+  input: './index.js',
+  output: {
+    file: './dist/sipoUtils.esm.js',
+    format: 'es'
+  },
+  plugins: []
+}
---
+++ rollup.config.umd.js
@@ -0,0 +1,9 @@
+export default {
+  input: './index.js',
+  output: {
+    file: './dist/sipoUtils.js',
+    format: 'umd',
+    name:'sipoUtils'
+  },
+  plugins: []
+}
