79ea48ed49c56d05d06f965eddc1be93c836acec
CJ-Zheng1023 <CJ_Zheng1023@hotmail.com>
2020-08-25 17:20:45 +0800

    base

--- src/highlight.js
+++ src/highlight.js
@@ -13,25 +13,71 @@ const _removeTag = targetStr => targetStr.replace(/(<span[^>]*>|<\/span>)/gi, ''
  */
 const _replaceArrow = targetStr => targetStr.replace(/<(?![^<>]+>)/gi, '&lt;').replace(/(?<!<[^<>]+)>/gi, '&gt;')
 /**
+ * 截词符转换成正则表达形式
+ * @param word   高亮关键词
+ * @private
+ */
+const _replaceTruncation = word => word.replace(/#/gi, '[a-zA-Z]').replace(/[?？]/gi, '.?')
+/**
+ * 关联高亮词转换成正则表达式
+ * @param word   高亮关键词
+ * @returns string
+ * @example 输入:A 3W B    输出:A.{3}B         输入:A 3D B   输出:(A.{3}B)|(B.{3}A)
+ * @private
+ */
+const _replaceRelation = word => {
+  let matches1 = word.match(/\s+(0|[1-9][0-9]*)?(w|W|d|D)\s+/gi)
+  if (!matches1 || !matches1.length) {
+    return word
+  }
+  let match = matches1[0]
+  //获取关联词
+  let [p1, p2] = word.split(match)
+  //获取关联运算符
+  let operation = match.trim()
+  let size = 0, type = ''
+  let matches2 = operation.match(/(0|[1-9][0-9]*)|(w|W|d|D)/gi)
+  if (matches2.length === 1) { //运算符不带步长的情况
+    [type] = matches2
+  } else {    //运算符带步长的情况
+    [size, type] = matches2
+  }
+  size = Number(size)
+  if (type.toUpperCase() === `W`) {
+    return `${p1}.{${size}}${p2}`
+  } else {
+    return `(${p1}.{${size}}${p2})|(${p2}.{${size}}${p1})`
+  }
+}
+/**
  * 执行高亮操作
  * @author zhengchj
  * @param targetStr     目标字符串
  * @param highlighters  高亮关键词集合
+ * @param truncatable   启用截词符高亮，默认值为true
+ * @param relatable     启用关联高亮，默认值为false
  */
-export default (targetStr, highlighters) => {
+export default (targetStr, highlighters, truncatable = true, relatable = false) => {
   let str = _removeTag(targetStr)
   str = _replaceArrow(str)
   highlighters.forEach(item => {
     let word = item.word
     const color = item.color
-    if (word) {
-      //替换高亮词中的<,>
-      word = word.replace(/</gi, '&lt;').replace(/>/gi, '&gt;')
-      //高亮关键词匹配规则,标签中的内容不可匹配
-      const regExp = new RegExp(`(?<!<[^<>]+)${word}(?![^<>]+>)`, 'gi')
-      //高亮关键词加高亮标签
-      str = str.replace(regExp, `<span style="color: ${color}">${word}</span>`)
+    if (!word) {
+      return
     }
+    //替换高亮词中的<,>
+    word = word.replace(/</gi, '&lt;').replace(/>/gi, '&gt;')
+    //高亮关键词匹配规则,标签中的内容不可匹配
+    if (truncatable) {
+      word = _replaceTruncation(word)
+    }
+    if (relatable) {
+      word = _replaceRelation(word)
+    }
+    const regExp = new RegExp(`((?<!<[^<>]+)${word}(?![^<>]+>))`, 'gi')
+    //高亮关键词加高亮标签
+    str = str.replace(regExp, `<span style="color: ${color}">$1</span>`)
   })
   return str
 }
--- test/unit/highlight.spec.js
+++ test/unit/highlight.spec.js
@@ -51,4 +51,37 @@ describe('高亮方法测试', () => {
     const tobe = `对一个<div>正则</div>表达式<span style="color: black">&gt;</span>模式`
     expect(utils.highlight(targetStr, highlighters)).toBe(tobe)
   })
+  test('截词符高亮', () => {
+    const targetStr = `对一个正则表达式模式spans and hello!`
+    const highlighters = [{
+      word: 'hello?',
+      color: 'blue'
+    }, {
+      word: 'spans',
+      color: 'black'
+    }]
+    const tobe = `对一个正则表达式模式<span style="color: black">spans</span> and <span style="color: blue">hello!</span>`
+    expect(utils.highlight(targetStr, highlighters, true)).toBe(tobe)
+  })
+  test('关联高亮,w运算符', () => {
+    const targetStr = `对一个正则表达式模式`
+    const highlighters = [{
+      word: '正 w  则',
+      color: 'blue'
+    }, {
+      word: '表 1w 式',
+      color: 'black'
+    }]
+    const tobe = `对一个<span style="color: blue">正则</span><span style="color: black">表达式</span>模式`
+    expect(utils.highlight(targetStr, highlighters, true, true)).toBe(tobe)
+  })
+  test('关联高亮,d运算符', () => {
+    const targetStr = `对一个正则表达式模式式应表`
+    const highlighters = [{
+      word: '表 1d  式',
+      color: 'blue'
+    }]
+    const tobe = `对一个正则<span style="color: blue">表达式</span>模式<span style="color: blue">式应表</span>`
+    expect(utils.highlight(targetStr, highlighters, true, true)).toBe(tobe)
+  })
 })
