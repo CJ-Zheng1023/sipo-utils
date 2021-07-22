2a4678d9136f33ef3169019b2e57fa77ffbb64f1
大唐圣僧 <tianhuifeis@126.com>
2021-07-22 20:09:08 +0800

    增加接口声明

--- types/focus.d.ts
+++ types/focus.d.ts
@@ -13,3 +13,12 @@ export interface Focuser{
  * @returns {string}                   执行聚焦后的字符串
  */
 export declare function focus(targetStr: string, focusers: Array<Focuser>): string
+
+/**
+ * 执行聚焦操作
+ * @author zhengchj
+ * @param {string} targetStr           目标字符串
+ * @param {Array<Focuser>} focusers    聚焦关键词集合
+ * @returns {string}                   执行聚焦后的字符串
+ */
+export declare function focusText(targetStr: string, focusers: Array<Focuser>): string
